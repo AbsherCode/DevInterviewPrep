0832229f512f188dc27eada3efccf367479c95ca
Marcus Absher <marcus.absher@gmail.com>
2026-08-11 12:24:16 -0700

    Initial Commit

---
+++ DevInterviewPrep/DevInterviewPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevInterviewPrepApp.swift
+//  DevInterviewPrep
+//
+//  Created by Marcus Absher on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DevInterviewPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
